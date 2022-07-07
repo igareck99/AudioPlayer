49bc6a2096c3e381c2193a04f37573bf9d412e86
Igor <igor9.i@yandex.ru>
2022-07-07 13:43:27 +0300

    Initial Commit

---
+++ MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Игорь Коноваленко on 07.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
